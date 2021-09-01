dcd18dd791445adeae97a79e5f899e4fd7411458
broezer <bruce@moerdjiman.com>
2021-09-01 23:15:43 +0200

    moiré01

---
+++ sketch_210901a.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  var w = width * 2;
+  var h = height * 2;
+  
+  translate(-w/4, -h/4);
+  for (var i=0; i< w; i+=10){
+    noFill();
+    //rectMode(CENTER);
+    ellipse(w/2,h/2, i);
+  }
+  
+  translate(w/200, 0);
+  for (var i=0; i< w; i+=10){
+    noFill();
+    //rectMode(CENTER);
+    ellipse(w/2,h/2, i);
+  }
+  
+  translate(-w/100, 0);
+  for (var i=0; i< w; i+=10){
+    noFill();
+    //rectMode(CENTER);
+    ellipse(w/2,h/2, i);
+  }
+  
+  noLoop();
+  
+  save("20210901.png");
+  
+}
